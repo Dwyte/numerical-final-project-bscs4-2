542a21bfe6b61a9a5632e852c2ab89ade059938d
dwyte <xdwightm@proton.me>
2023-06-29 17:35:58 +0800

    Added newton raphson

--- openMethod/script.js
+++ openMethod/script.js
@@ -1,34 +1,39 @@
+function handleSelectChange() {
+  let methodInput = document.forms["form"]["method"].value;
+  if (methodInput === "newton") {
+    document.getElementById("initialGuessInputLabel").style.display = "block";
+    document.getElementById("xLeftInitialInputLabel").style.display = "none";
+    document.getElementById("xRightInitialInputLabel").style.display = "none";
+  } else {
+    document.getElementById("initialGuessInputLabel").style.display = "none";
+    document.getElementById("xLeftInitialInputLabel").style.display = "block";
+    document.getElementById("xRightInitialInputLabel").style.display = "block";
+  }
+}
+
 function handleSubmit() {
   try {
     let equationInput = document.forms["form"]["equation"].value;
     let methodInput = document.forms["form"]["method"].value;
     let precisionInput = parseFloat(document.forms["form"]["precision"].value);
-    let xLeftInitial = parseFloat(document.forms["form"]["xLeftInitial"].value);
-    let xRightInitial = parseFloat(
-      document.forms["form"]["xRightInitial"].value
-    );
 
-    const methods = {
-      secant: secantMethod,
-      newton: newtonRaphsonMethod,
-    };
+    let tableHeader;
+    if (methodInput === "secant") {
+      let xLeftInitial = parseFloat(
+        document.forms["form"]["xLeftInitial"].value
+      );
+      let xRightInitial = parseFloat(
+        document.forms["form"]["xRightInitial"].value
+      );
 
-    const { root, iterations } = methods[methodInput](
-      equationInput,
-      precisionInput,
-      xLeftInitial,
-      xRightInitial
-    );
+      answer = secantMethod(
+        equationInput,
+        precisionInput,
+        xLeftInitial,
+        xRightInitial
+      );
 
-    const decimalPlaces = Math.log10(1 / precisionInput);
-    console.log(decimalPlaces);
-
-    document.getElementById("solutions").innerHTML = `x ≈ ${root.toFixed(
-      decimalPlaces
-    )}`;
-    console.log(iterations);
-    displayIterationsTable(
-      [
+      tableHeader = [
         "i",
         "X<sub>a</sub>",
         "X<sub>b</sub>",
@@ -36,7 +41,35 @@ function handleSubmit() {
         "f(X<sub>b</sub>)",
         "X<sub>n</sub>",
         "f(X<sub>n</sub>)",
-      ],
+      ];
+    } else {
+      let initialGuess = parseFloat(
+        document.forms["form"]["initialGuess"].value
+      );
+
+      answer = newtonRaphsonMethod(equationInput, precisionInput, initialGuess);
+
+      tableHeader = [
+        "i",
+        "X",
+        "f(X)",
+        "f'(X)",
+        "X<sub>n</sub>",
+        "f(X<sub>n</sub>)",
+      ];
+    }
+
+    const { root, iterations } = answer;
+
+    const decimalPlaces = Math.log10(1 / precisionInput);
+    console.log(decimalPlaces);
+
+    document.getElementById("solutions").innerHTML = `x ≈ ${root.toFixed(
+      decimalPlaces
+    )}`;
+
+    displayIterationsTable(
+      tableHeader,
       iterations.map((iteration) =>
         iteration.map((num) => num.toFixed(decimalPlaces))
       )
@@ -79,8 +112,40 @@ function secantMethod(equation, precision, x0, x1, maxIterations = 100) {
   return { root: x1, iterations };
 }
 
-function newtonRaphsonMethod(equation, precision, x0, x1, maxIterations = 100) {
-  //
+function newtonRaphsonMethod(
+  equation,
+  precision,
+  initialGuess,
+  _,
+  maxIterations = 100
+) {
+  // Define the function and its derivative based on the equation string
+  const f = math.parse(equation).compile();
+  const fDerivative = math.derivative(equation, "x").compile();
+
+  let iterations = [];
+
+  let x = initialGuess;
+
+  // Iterate until the desired precision is achieved or maximum iterations reached
+  for (let i = 0; i < maxIterations; i++) {
+    const fX = f.evaluate({ x });
+    const fXDerivative = fDerivative.evaluate({ x });
+
+    const xNext = x - fX / fXDerivative; // Compute the next approximation
+
+    let iteration = [x, fX, fXDerivative, xNext, f.evaluate({ x: xNext })];
+    iterations.push(iteration);
+
+    if (Math.abs(xNext - x) < precision) {
+      return { root: xNext, iterations };
+    }
+
+    x = xNext;
+  }
+
+  // Return the approximate root within the desired precision
+  return { root: x, iterations };
 }
 
 function displayIterationsTable(header, body) {
@@ -99,6 +164,5 @@ function displayIterationsTable(header, body) {
     }
     table += "</tr>";
   }
-  console.log(table);
   document.getElementById("table").innerHTML = table;
 }
